518191e6addca100134e2410c24f24547fc19ac7
TahsinUchiha <alton255@yahoo.co.uk>
2018-10-31 16:58:28 +0000

    sql stuff

--- src/main/java/com/qa/accountapp/management/Account.java
+++ src/main/java/com/qa/accountapp/management/Account.java
@@ -1,9 +1,30 @@
 package com.qa.accountapp.management;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Account {
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	@Column(length = 100)
 	private String firstName;
+	@Column(length = 50)
 	private String lastName;
+	@Column(length = 4)
 	private int accountNumber;
+	
+	
+	
+	
+	private String title;
+	
+	private String genre;
+
+
 
 	public Account(String firstName, String lastName, int accountNumber) {
 		super();
